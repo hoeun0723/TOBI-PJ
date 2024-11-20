199531680c6bc0b7459d3df1095e0963ca76e9b9
sumhillj <sumhillj@gmail.com>
2024-11-20 23:30:23 +0900

    feat: Status 컴포넌트

--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -1,9 +1,38 @@
 import * as S from './Status.style';
+import ledIcon from '../../assets/led.png';
+import soilIcon from '../../assets/soil.png';
+import sunIcon from '../../assets/sun.png';
+import waterIcon from '../../assets/water.png';
 
 function Status () {
   return (
     <S.StatusContainer>
-      
+      <S.Container>
+        <S.Status>
+          <S.StatusIcon imageUrl={waterIcon}/>
+          <S.StatusTitle>토양습도:</S.StatusTitle>
+          <S.StatusFigure>50%</S.StatusFigure>
+        </S.Status>
+        <S.Status>
+          <S.StatusIcon imageUrl={soilIcon}/>
+          <S.StatusTitle>토양온도:</S.StatusTitle>
+          <S.StatusFigure>22°C</S.StatusFigure>
+        </S.Status>
+        <S.Status>
+          <S.StatusIcon imageUrl={sunIcon}/>
+          <S.StatusTitle>햇빛:</S.StatusTitle>
+          <S.StatusFigureStrong>강함</S.StatusFigureStrong>
+        </S.Status>
+        <S.Status>
+          <S.StatusIcon imageUrl={ledIcon}/>
+          <S.StatusTitle>LED:</S.StatusTitle>
+          <S.StatusFigureStrong>5단계</S.StatusFigureStrong>
+        </S.Status>
+      </S.Container>
+      <S.Container2>
+        <S.Title>나의 식물 종류</S.Title>
+        <S.PlantName>산세베리아</S.PlantName>
+      </S.Container2>
     </S.StatusContainer>
   )
 }
--- src/components/Status/Status.style.js
+++ src/components/Status/Status.style.js
@@ -14,3 +14,105 @@ export const StatusContainer = styled.div`
   background-position: center;
   z-index: 0;
 `
+
+export const Container = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 21px;
+  margin-top: 40px;
+`
+
+export const Container2 = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+`
+
+export const Title = styled.div`
+  color: #000000;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const PlantName = styled.div`
+  color: #1FAA67;
+  font-size: 32px;
+  font-weight: 700;
+  margin-top: 10px;
+`
+
+export const Status = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 248px;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+`
+
+export const StatusIcon = styled.div`
+  width: 48px; 
+  height: 48px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+  position: absolute;
+  left: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const StatusTitle = styled.div`
+  height: 48px;
+  color: #000000;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 400;
+  position: relative;
+  left: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const StatusFigure = styled.div`
+  height: 48px;
+  color: #000000;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 5px;
+  position: relative;
+  left: 10px;
+`
+
+export const StatusFigureStrong = styled.div`
+  height: 48px;
+  color: #000000;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 5px;
+  position: relative;
+  left: 10px;
+`
